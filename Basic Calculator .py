438af1cb3a6ffedc2dc969f4a40778f3172bcd03
Yashaswa Alguje <yashaswaalguje07@gmail.com>
2025-09-08 10:30:32 +0530

    Add files via upload

---
+++ Basic Calculator .py
@@ -0,0 +1,34 @@
+
+print("Basic Calculator")
+
+# float is used for inputing decimal value 
+a = float(int(input(str("Enter First Number:"))))
+b = float(int(input(str("Enter second number: "))))      
+
+
+#this all are operation used 
+def calculator(a,b):
+    symbol = input("Enter operation (+, -, *, /,//,%): ")
+
+    if symbol == "+":
+        print(f"The Sum of {a} and {b} is "+str(a + b))
+    elif symbol == "-":
+        print(f"The Substraction of {a}  and {b} is "+str(a - b))
+    elif symbol == "*":
+        print(f"The Multiplication of {a} and {b} is "+str(a * b))
+    elif symbol == "/":
+        print(f"The Division of {a} and {b} is "+str(a / b))
+    elif symbol == "//":
+        print(f"The float of {a}  and {b} is "+str(a // b))
+    elif symbol == "%":
+        print(f"The Mode of {a}  and {b} is "+str(a % b))
+    else:
+        print("Invalid Approch, Plese Try Again")
+        calculator(a,b)
+#elif,if,else are used for
+
+    # Thank You 
+    #Be The Change 
+
+    
+calculator(a,b)
